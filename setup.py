1f2db14f7e236f6161ff84788353b112dedb74e8
Sebastian Junges <sebastian.junges@gmail.com>
2020-06-02 20:03:21 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name="gridstorm",
+    version="0.1",
+    packages=find_packages(),
+    scripts=[],
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    #install_requires=["docutils>=0.3"],
+    package_data={
+        # If any package contains *.nm or *.rst files, include them:
+        "": ["*.nm", "*.rst"],
+    },
+
+    # metadata to display on PyPI
+    # author omitted for anonymity
+    description="This is a benchmark set visualiser for simulating grid worlds with storm.",
+    keywords="gridworld storm model-checking",
+)
